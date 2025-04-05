268eef0ee4e51a50c911edc500c1f73eddaba5d0
martinotin24 <72896552+martinotin24@users.noreply.github.com>
2025-04-04 17:03:41 -0700

    Update party_planner.py

--- party_planner.py
+++ party_planner.py
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Digital Party Planner</title>
+</head>
+<body>
+    <h1>Digital Party Planner</h1>
+    <form action="/party_planner.py" method="get">
+        <p>Select the party items:</p>
+        <input type="checkbox" name="items" value="0"> Cake<br>
+        <input type="checkbox" name="items" value="1"> Balloons<br>
+        <input type="checkbox" name="items" value="2"> Music System<br>
+        <input type="checkbox" name="items" value="3"> Lights<br>
+        <input type="checkbox" name="items" value="4"> Catering Service<br>
+        <input type="checkbox" name="items" value="5"> DJ<br>
+        <input type="checkbox" name="items" value="6"> Photo Booth<br>
+        <input type="checkbox" name="items" value="7"> Tables<br>
+        <input type="checkbox" name="items" value="8"> Chairs<br>
+        <input type="checkbox" name="items" value="9"> Drinks<br>
+        <input type="checkbox" name="items" value="10"> Party Hats<br>
+        <input type="checkbox" name="items" value="11"> Streamers<br>
+        <input type="checkbox" name="items" value="12"> Invitation Cards<br>
+        <input type="checkbox" name="items" value="13"> Party Games<br>
+        <input type="checkbox" name="items" value="14"> Cleaning Service<br><br>
+        
+        <input type="submit" value="Plan My Party">
+    </form>
+</body>
+</html>
 
